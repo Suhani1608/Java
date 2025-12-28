e34d6b77a76f050a5457a6a93aabc66b6cd01ccb
Suhani1608 <9835suhani@gmail.com>
2025-12-28 16:46:15 +0530

    Add files via upload

---
+++ digitSum.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class digitSum {
+
+    public static int sum(int n){
+        if(n < 10){
+            return n;
+        }else{
+            return (n%10)+sum(n/10);
+        }
+    }
+    public static void main(String[] args){
+        System.out.println(sum(345));
+    }
+}
