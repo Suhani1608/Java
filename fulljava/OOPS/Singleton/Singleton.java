2c34a366d4817e03e8f1bf3d105a2cdf9e6566a3
Suhani1608 <9835suhani@gmail.com>
2026-01-29 17:20:23 +0530

    Add files via upload

---
+++ fulljava/OOPS/Singleton/Singleton.java
@@ -0,0 +1,20 @@
+package OOPS.Singleton;
+
+public class Singleton {
+    
+
+    private Singleton() {
+    
+    }
+    private static Singleton instance;
+     public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+       
+        return instance ;
+
+     }
+
+   
+}
--- fulljava/OOPS/Singleton/main.java
+++ fulljava/OOPS/Singleton/main.java
@@ -1,9 +1,14 @@
-package OOPS.StaticExample;
+package OOPS.Singleton;
 
+/**
+ * Main class demonstrating Singleton pattern usage
+ */
 public class main {
-    public static void main(String[] args) {
-        Human rajeev = new Human(22, "aashka", false, 123456, 0);
-        Human rahul = new Human(20, "suhani", false, 123456, 0);
-        System.out.println("Total population: " + Human.population);
+    public static void main (String[] args) {
+        // Get singleton instance
+        Singleton x= Singleton.getInstance();
+        Singleton y = Singleton.getInstance();
+        Singleton z = Singleton.getInstance();
+     
     }
 }
